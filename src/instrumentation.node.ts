4c6f3bff3f66354a149a1cb54e0f27ae64e3a861
Jim Anderson <jim.anderson@okta.com>
2024-10-17 09:02:21 -0500

    simplify

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -2,9 +2,10 @@ import { NodeSDK } from "@opentelemetry/sdk-node";
 import {
     HttpInstrumentation
 } from '@opentelemetry/instrumentation-http';
-import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+
+// Looks like the trace exporter is experimental
+// https://www.npmjs.com/package/@opentelemetry/exporter-trace-otlp-proto
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
 
 const sdk = new NodeSDK({
   traceExporter:  new OTLPTraceExporter({
@@ -13,17 +14,9 @@ const sdk = new NodeSDK({
     // optional - collection of custom headers to be sent with each request, empty by default
     headers: {},
   }),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter({
-      //url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-      headers: {}, // an optional object containing custom headers to be sent with each request
-      concurrencyLimit: 1, // an optional limit on pending requests
-    }),
-  }),
   instrumentations: [
     new HttpInstrumentation({
       requestHook: (span, reqInfo: any) => {
-        console.log(`***** reqInfo: ${JSON.stringify(reqInfo)}`);
         span.updateName( `${reqInfo.method} ${reqInfo.url || reqInfo.path}`);
       },
     }),
